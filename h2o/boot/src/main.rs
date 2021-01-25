a0fd92920c5743b24f16cc888bd355a3e32b2b0b
徐启航 <akucxy@163.com>
2021-01-25 15:36:51 +0800

    Get Modes

--- h2o/boot/src/main.rs
+++ h2o/boot/src/main.rs
@@ -6,11 +6,29 @@
 
 use log::*;
 use uefi::prelude::*;
+use uefi::proto::*;
 
 #[entry]
 fn efi_main(_img: Handle, syst: SystemTable<Boot>) -> Status {
       uefi_services::init(&syst).expect_success("Failed to initialize EFI services");
       info!("hello world!");
+
+      let gop = syst
+            .boot_services()
+            .locate_protocol::<console::gop::GraphicsOutput>()
+            .expect_success("Failed to locate GOP");
+
+      let modes = unsafe { &*gop.get() }.modes();
+      for mode in modes {
+            let mode = mode.unwrap();
+
+            let minfo = mode.info();
+            let res = minfo.resolution();
+            let pix = minfo.pixel_format();
+            let stride = minfo.stride();
+
+            info!("Mode: {}x{}: {:?} ({})", res.0, res.1, pix, stride);
+      }
       loop {
             unsafe { asm!("pause") }
       }
