e1ad5361c9df00450cc65fcbefed526f0272924c
Js2xxx <akucxy@163.com>
2023-02-20 13:38:43 +0800

    Adapt to qemu Q35 & kernel time simplification (#8)
    * Adapt to qemu Q35 & kernel time simplification
    
    * Update format

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,12 +4,12 @@ if [ $1 = "qemu" ]; then
       qemu-system-x86_64 -L /usr/share/ovmf -bios OVMF.fd \
             -m 4096 -cpu max -smp $2 -serial file:debug/qemu.log \
             -drive format=raw,file=target/img/efi.img -boot c \
-            -monitor stdio $3 $4 $5 $6 $7 $8 $9
+            -monitor stdio -M q35 $3 $4 $5 $6 $7 $8 $9
 elif [ $1 = "qdbg" ]; then
       qemu-system-x86_64 -L /usr/share/ovmf -bios OVMF.fd \
             -m 4096 -cpu max -smp $2 -serial file:debug/qemu.log \
             -drive format=raw,file=target/img/efi.img -boot c \
-            -monitor stdio -s -S $3 $4 $5 $6 $7 $8 $9
+            -monitor stdio -M q35 -s -S $3 $4 $5 $6 $7 $8 $9
 elif [ $1 = "vbox" ]; then
     /usr/lib/virtualbox/VirtualBoxVM --startvm "OV3" --dbg $2 $3 $4 $5 $6 $7 $8 $9
 elif [ $1 = "vmware" ]; then
